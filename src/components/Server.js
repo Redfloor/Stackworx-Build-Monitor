89ac1d3bbc7d246b020d874d1fbf96afd650b9e2
Dylan <redfloordevelopment@gmail.com>
2018-01-14 16:19:53 +0200

    Style commits
    Got the basic layout and styling down - just about everything in place for the functionality.

--- src/components/Server.js
+++ src/components/Server.js
@@ -4,6 +4,7 @@ const Server = (props) => {
   return (
     <li className="server-item">
       <p>{props.server}</p>
+      <p>Status: <span className="status-text">STATUS</span></p>
       <a href={props.server} target="_blank">Visit</a>
     </li>
   );
