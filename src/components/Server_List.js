e52fe2460876aecbfbbf89c55fb9b04ba66f4ab5
Dylan <redfloordevelopment@gmail.com>
2018-01-14 16:52:14 +0200

    removed http-request
    Removed http-request in favor of async

--- src/components/Server_List.js
+++ src/components/Server_List.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import Server from './Server';
-import Request from 'react-http-request';
 
 export default class ServerList extends Component  {
-  // const Servers = props.videos.map( (server) => {
-  //   return <VideoListItem key={ video.etag } video = { video } />;
-  // } );
 
   render() {
     // console.log(this.props.servers);
@@ -15,25 +11,6 @@ export default class ServerList extends Component  {
       <ul className="server-list">
         {serverListHTML}
       </ul>
-
-      // <Request
-      //   url='https://api.github.com/users/mbasso'
-      //   method='get'
-      //   accept='application/json'
-      //   verbose={true}
-      // >
-      //   {
-      //     ({error, result, loading}) => {
-      //       if (loading) {
-      //         return <div>loading...</div>;
-      //       } else {
-      //         return <div>{ JSON.stringify(result) }</div>;
-      //       }
-      //     }
-      //   }
-      // </Request>
-
-
     );
   }
 }
